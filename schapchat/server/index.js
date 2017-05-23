44ff240738483f3d695184c0d3b97e91885ed0b3
Jonathan Schapiro <jschapiro@opentable.com>
2017-05-22 20:46:12 -0700

    begin google api auth

--- schapchat/server/index.js
+++ schapchat/server/index.js
@@ -1,42 +1,53 @@
 //first we import our dependencies…
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-//and create our instances
-var app = express();
-var router = express.Router();
-//set our port to either a predetermined port number if you have set 
-//it up, or 3001
-var port = process.env.API_PORT || 3001;
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-mongoose.connect('', (err) => {
+const app = express();
+const router = express.Router();
+const port = process.env.API_PORT || 3001;
+
+const {dbUser, dbPassword, dbUrl} = require('../config/database');
+const {web} = require('../config/youtube');
+const {client_id, project_id, auth_uri, token_uri, client_secret} = web;
+
+const DB_CONNECTION_URL = `mongodb://${dbUser}:${dbPassword}${dbUrl}`;
+
+const google = require('googleapis');
+
+const OAuth2 = google.auth.OAuth2;
+
+var oauth2Client = new OAuth2(
+  client_id,
+  client_secret,
+  'localhost:3001/api/google/callback'
+);
+
+mongoose.connect(DB_CONNECTION_URL, (err) => {
 	if (err) {
 		return console.error(new Error('A problem occured while connecting to database!'));
 	}
 	console.log('Succesfully connected to database');
 });
-//now we should configure the API to use bodyParser and look for 
-//JSON data in the request body
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-//To prevent errors from Cross Origin Resource Sharing, we will set 
-//our headers to allow CORS with middleware like so:
+
 app.use(function(req, res, next) {
  res.setHeader('Access-Control-Allow-Origin', '*');
  res.setHeader('Access-Control-Allow-Credentials', 'true');
  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT,DELETE');
  res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-//and remove cacheing so we get the most recent comments
  res.setHeader('Cache-Control', 'no-cache');
  next();
 });
-//now we can set the route path & initialize the API
+
 router.get('/', function(req, res) {
  res.json({ message: 'API Initialized!'});
 });
-//Use our router configuration when we call /api
+
 app.use('/api', router);
-//starts the server and listens for requests
+
 app.listen(port, function() {
- console.log(`api running on port ${port}`);
+ console.log(`Prankin' on ${port}`);
 });
